08abf2e0cefe79dacee97a6f0656b1fda1fc47a7
ezzimo <ezzi.mohammed1@yahoo.fr>
2020-11-23 02:38:33 +0100

    Comments and adding exit() method from sys object to stop the execution.

--- Python_Github_API.py
+++ Python_Github_API.py
@@ -1,5 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 import json
+import sys
 from collections import Counter
 from datetime import datetime, timedelta, date
 from LanguagesRepoCounter import LanguagesCounter
@@ -7,6 +8,7 @@ from RepoListLanguages import RepoListLanguages
 from tabulate import tabulate
 
 
+#getting the date of one mounth ago from today
 One_Mouth_ago_date = datetime.date(datetime.now()) - timedelta(days=30)
 
 url = 'https://api.github.com/search/repositories?q=created:%3E'+One_Mouth_ago_date.isoformat()+'&sort=stars&order=desc&page=1&per_page=100'
@@ -19,21 +21,27 @@ try:
 	json_data = json.loads(unicoded_data)
 except:
 	json_data = None
-
+# testing retrieaved data
 if not json_data or json_data["total_count"]<1:
-	print('######## Failure To Retrieve ########')
 	print(json_data)
+	sys.exit('######## Failure To Retrieve ########')
 
 
 
+#Counting the number of repo using every language
 Lang_Dic_Count = LanguagesCounter(json_data)
 
+# Getting the list of repo using every language
 DicDataList = RepoListLanguages(json_data, Lang_Dic_Count)
+
+# Displaye the repo's urls for every language
 for key in DicDataList:
 	print(key, ' language used in')
 	for i in range(len(DicDataList[key])):
 		print("                           ", DicDataList[key][i])
 
+
+# Displaye the number of repos using every language in a table 
 table = []
 for key in Lang_Dic_Count:
 	under_table=[]
